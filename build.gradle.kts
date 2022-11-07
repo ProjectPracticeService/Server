2904334c8b553baa27a79ee32b295db083e1427f
transaero21 <martynenkodima21@gmail.com>
2022-11-07 12:40:17 +0300

    update some libs and define versions for them

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val kotlinVersion = "1.7.20"
+val springBootVersion = "2.7.5"
+val jacksonVersion = "2.14.0"
+val yamlVersion = "1.33"
+
 plugins {
 	id("org.springframework.boot") version "2.7.5"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.7.20"
+	kotlin("plugin.spring") version "1.7.20"
 }
 
-group = "com.example"
+group = "ru.mephi"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
@@ -16,12 +21,20 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// Kotlin
+	implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+
+	// Spring Boot
+	implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+	developmentOnly("org.springframework.boot:spring-boot-devtools:$springBootVersion")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
+
+	// Jackson
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+
+	// YAML
+	implementation("org.yaml:snakeyaml:$yamlVersion")
 }
 
 tasks.withType<KotlinCompile> {
